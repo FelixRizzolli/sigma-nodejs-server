be2989c8a8095bfbc9241ba678adb25db4aa12f0
Felix Rizzolli <felix.rizzolli@outlook.de>
2023-06-26 17:27:00 +0200

    add new LessonContentTypes: UNORDEREDLIST and ORDEREDLIST

--- src/models/Lessons/Lesson.ts
+++ src/models/Lessons/Lesson.ts
@@ -2,7 +2,9 @@ import mongoose, { Schema, SchemaOptions } from 'mongoose';
 
 enum LessonContentTypes {
     Quote = 'QUOTE',
-    Paragraph = 'PARAGRAPH'
+    Paragraph = 'PARAGRAPH',
+    UnorderedList = 'UNORDEREDLIST',
+    OrderedList = 'ORDEREDLIST'
 }
 
 const lessonContentSchema = new Schema({
@@ -15,13 +17,15 @@ const lessonContentSchema = new Schema({
     source: { type: String, required: false },
     author: { type: String, required: false },
     paragraph: { type: String, required: false },
+    intro: { type: String, required: false },
+    list: { type: [String], required: false },
 });
 
 
 const lessonSchema = new Schema({
     title: { type: String, required: true },
     number: { type: Number, required: true },
-    content: [lessonContentSchema],
+    content: { type: [lessonContentSchema], required: false},
 });
 
 export const LessonModel = mongoose.model('Lesson', lessonSchema);
